0fcc1bd5b2eab91106e75c54d6ec0c7feccd383a
Aejkatappaja <joverjlj@gmail.com>
2023-10-05 22:01:03 +0200

    delete: unused files

--- hello_cargo/target/debug/deps/hello_cargo-2b11a0ad5f138b14.d
+++
@@ -1,5 +0,0 @@
-/Users/frank/Projects/Rust/hello_cargo/target/debug/deps/hello_cargo-2b11a0ad5f138b14.rmeta: src/main.rs
-
-/Users/frank/Projects/Rust/hello_cargo/target/debug/deps/hello_cargo-2b11a0ad5f138b14.d: src/main.rs
-
-src/main.rs:
