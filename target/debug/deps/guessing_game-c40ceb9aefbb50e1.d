3049cbe63c51a9cc975d55f4bd805db215070a8a
Aejkatappaja <joverjlj@gmail.com>
2023-10-05 22:22:55 +0200

    chore: rework repo !

--- target/debug/deps/guessing_game-c40ceb9aefbb50e1.d
+++ target/debug/deps/guessing_game-c40ceb9aefbb50e1.d
@@ -1,5 +1,5 @@
-/Users/frank/Projects/Rust/guessing_game/target/debug/deps/guessing_game-c40ceb9aefbb50e1.rmeta: src/main.rs
+/Users/frank/Projects/Rust/target/debug/deps/guessing_game-c40ceb9aefbb50e1.rmeta: src/main.rs
 
-/Users/frank/Projects/Rust/guessing_game/target/debug/deps/guessing_game-c40ceb9aefbb50e1.d: src/main.rs
+/Users/frank/Projects/Rust/target/debug/deps/guessing_game-c40ceb9aefbb50e1.d: src/main.rs
 
 src/main.rs:
--- target/debug/deps/rand_chacha-1102f7dd79f424de.d
+++ target/debug/deps/rand_chacha-1102f7dd79f424de.d
@@ -1,6 +1,6 @@
-/Users/frank/Projects/Rust/guessing_game/target/debug/deps/rand_chacha-1102f7dd79f424de.rmeta: /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
+/Users/frank/Projects/Rust/target/debug/deps/rand_chacha-1102f7dd79f424de.rmeta: /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
 
-/Users/frank/Projects/Rust/guessing_game/target/debug/deps/rand_chacha-1102f7dd79f424de.d: /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
+/Users/frank/Projects/Rust/target/debug/deps/rand_chacha-1102f7dd79f424de.d: /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
 
 /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs:
 /Users/frank/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs:
